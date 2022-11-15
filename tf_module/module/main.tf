14b5cffbe7b3f43d7924ca2af5f235bc76600863
yaaraverner <yaara@vernermail.com>
2022-11-15 15:09:06 +0200

    create nested modules

--- tf_module/module/main.tf
+++ tf_module/module/main.tf
@@ -1,11 +1,4 @@
-locals {
-  bucket = var.bucket
-}
-
-resource "aws_s3_bucket_public_access_block" "var_bucket" {
-  bucket                  = local.bucket
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+module "inner_s3_module" {
+  source = "./module2"
+  bucket2 = var.bucket
 }
